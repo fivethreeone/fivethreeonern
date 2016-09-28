504c39c65e34583c16162dc1c61f27517b115af5
Frank Wang <frank.wangtao@gmail.com>
2016-09-28 19:47:04 -0400

    Initial project setup with sqlite

---
+++ android/app/src/main/java/com/fivethreeonern/MainActivity.java
@@ -0,0 +1,26 @@
+package com.fivethreeonern;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "fivethreeonern";
+    }
+
+   /**
+   * A list of packages used by the app. If the app uses additional views
+   * or modules besides the default ones, add more packages here.
+   */
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new SQLitePluginPackage(),   // register SQLite Plugin here
+        new MainReactPackage());
+    }
+}
---
+++ src/fivethreeonern/ios/core.cljs
@@ -0,0 +1,43 @@
+(ns fivethreeonern.ios.core
+  (:require [reagent.core :as r :refer [atom]]
+            [re-frame.core :refer [subscribe dispatch dispatch-sync]]
+            [fivethreeonern.events]
+            [fivethreeonern.subs]))
+
+(def ReactNative (js/require "react-native"))
+
+(def sql (js/require "react-native-sqlite-storage"))
+
+(def app-registry (.-AppRegistry ReactNative))
+(def text (r/adapt-react-class (.-Text ReactNative)))
+(def view (r/adapt-react-class (.-View ReactNative)))
+(def image (r/adapt-react-class (.-Image ReactNative)))
+(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
+
+(def db (.openDatabase sql
+                       "test.db"
+                       "1.0"
+                       "Test Database"
+                       200000
+                       #(js/console.log "ok")
+                       #(js/console.log "not ok")))
+
+(def logo-img (js/require "./images/cljs.png"))
+
+(defn alert [title]
+      (.alert (.-Alert ReactNative) title))
+
+(defn app-root []
+  (let [greeting (subscribe [:get-greeting])]
+    (fn []
+      [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
+       [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
+       [image {:source logo-img
+               :style  {:width 80 :height 80 :margin-bottom 30}}]
+       [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
+                             :on-press #(alert "HELLO!")}
+        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
+
+(defn init []
+      (dispatch-sync [:initialize-db])
+      (.registerComponent app-registry "fivethreeonern" #(r/reactify-component app-root)))
